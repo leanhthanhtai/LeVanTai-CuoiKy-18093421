7f0918326e998d32b41217f044fc8937b74c555e
leanhthanhtai <leanhthanhtai@gmail.com>
2021-12-16 16:15:49 +0700

    cacheable-update-delete-find-post

--- src/main/java/com/example/levantai18093421toyota/LeVanTai18093421ToyotaApplication.java
+++ src/main/java/com/example/levantai18093421toyota/LeVanTai18093421ToyotaApplication.java
@@ -2,8 +2,10 @@ package com.example.levantai18093421toyota;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LeVanTai18093421ToyotaApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/levantai18093421toyota/controllers/AppController.java
+++ src/main/java/com/example/levantai18093421toyota/controllers/AppController.java
@@ -4,6 +4,7 @@ import com.example.levantai18093421toyota.Interfaces.ToyotaInterface;
 import com.example.levantai18093421toyota.Repositorys.ToyotaRepository;
 import com.example.levantai18093421toyota.entity.Toyota;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,24 +15,26 @@ public class AppController {
     @Autowired
     ToyotaInterface toyotaInterface;
 
+    @Cacheable(value = "GetAll")
     @GetMapping("/toyota")
     public List<Toyota> GetAll(){
         return  toyotaInterface.GetList();
     }
 
+    @Cacheable(value = "Post")
     @PostMapping("/toyota")
     public String  Post(@RequestBody Toyota toyota){
         toyota.setId(0);
-
         toyotaInterface.AddToyota(toyota);
         return "Add success";
     }
-
+    @Cacheable(value = "PutTo")
     @PutMapping("/toyota")
     public String  PutTo(@RequestBody Toyota toyota){
         toyotaInterface.Update(toyota);
         return "UPdate success";
     }
+    @Cacheable(value = "DeleteTo")
     @DeleteMapping("/toyota/{id}")
     public String  DeleteTo(@PathVariable long id){
         toyotaInterface.Delete(id);
